9538a808e33b61ec7609150f49d0c072477b2fb7
Remi <remi.shergold@hotmail.com>
2015-03-13 16:50:00 +0000

    Initial Commit

---
+++ Testing With Quick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing With Quick
+//
+//  Created by Hatch on 13/03/2015.
+//  Copyright (c) 2015 Hatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Testing With QuickTests/Testing_With_QuickTests.swift
@@ -0,0 +1,36 @@
+//
+//  Testing_With_QuickTests.swift
+//  Testing With QuickTests
+//
+//  Created by Hatch on 13/03/2015.
+//  Copyright (c) 2015 Hatch. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Testing_With_QuickTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
