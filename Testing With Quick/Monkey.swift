5f9759448e9c56c0bdf1998c2c213e5140e68607
Remi <remi.shergold@hotmail.com>
2015-03-16 12:26:18 +0000

    Created silly monkeys. Got unexpected test failure message

---
+++ Testing With Quick/Monkey.swift
@@ -0,0 +1,29 @@
+//
+//  Monkey.swift
+//  Testing With Quick
+//
+//  Created by Hatch on 16/03/2015.
+//  Copyright (c) 2015 Hatch. All rights reserved.
+//
+
+import Foundation
+
+class Monkey {
+  
+  var name: String
+  var silliness: Silliness
+  
+  enum Silliness {
+    case NotSilly, VerySilly, ExtremelySilly
+  }
+  
+  init(name:String, silliness:Silliness) {
+    self.name = name
+    self.silliness = silliness
+  }
+  
+}
+
+func silliest(monkeys: [Monkey]) -> [Monkey] {
+  return monkeys.filter { $0.silliness == .VerySilly }
+}
---
+++ Testing With QuickTests/MonkeySpec.swift
@@ -0,0 +1,51 @@
+//
+//  MonkeySpec.swift
+//  Testing With Quick
+//
+//  Created by Hatch on 16/03/2015.
+//  Copyright (c) 2015 Hatch. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+
+class MonkeySpec: QuickSpec {
+  override func spec() {
+    describe("a Monkey") {
+      
+      var monkey:Monkey!
+      beforeEach {
+        monkey = Monkey(name: "Pete", silliness: .ExtremelySilly)
+      }
+      
+      it("should have a name") {
+        expect(monkey.name).to(equal("Pete"))
+      }
+      
+      it("should have a silliness level") {
+        expect(monkey.silliness).to(equal(Monkey.Silliness.ExtremelySilly))
+      }
+      
+    }
+    
+    describe("silliest monkeys") {
+      
+      it("should contain 'Kiki'") {
+        let kiki = Monkey(name: "Kiki", silliness: .ExtremelySilly)
+        let carl = Monkey(name: "Carl", silliness: .NotSilly)
+        let jane = Monkey(name: "Jane", silliness: .VerySilly)
+        let sillyMonkeys = silliest([kiki, carl, jane])
+        expect(sillyMonkeys).to(contain(kiki))
+        
+        /* got message
+            failed - expected to contain <Optional(Testing_With_QuickTests.Monkey)>, got <("Testing_With_QuickTests.Monkey")>
+          expected
+            failed - expected to contain <Monkey(name: Kiki, silliness: ExtremelySilly)>, got <[Monkey(name: Jane, silliness: VerySilly)]>
+        */
+      }
+      
+    }
+    
+  }
+}
